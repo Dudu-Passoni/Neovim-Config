84cd66d9b217314ac4b493433c70fa3d9b8e17f5
Luis Eduardo Passoni <115907714+Dudu-Passoni@users.noreply.github.com>
2024-11-21 20:53:07 -0300

    Update maps.lua

--- lua/dudupassoni/maps.lua
+++ lua/dudupassoni/maps.lua
@@ -9,3 +9,8 @@ vim.keymap.set("n", "<leader>ff", "<CMD>Telescope find_files<CR>")
 -- Neotree (tbm n vai)
 
 vim.keymap.set("n", "<leader>t", "<CMD>Neotree<CR>")
+
+-- Mover texto selecionado para cima e para baixo no modo visual
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Mover seleção para baixo" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Mover seleção para cima" })
