03d792bb7eee9233ff31b2ae1af88eea40378fc5
Luis Eduardo Passoni <dudupassoni@gmail.com>
2024-11-30 18:29:20 -0300

    conseall

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require("dudupassoni.settings")
 require("dudupassoni.maps")
 require("dudupassoni.lazy")
 vim.opt.clipboard = "unnamedplus"
+vim.opt.conceallevel = 2 -- ou 2, dependendo da sua preferência
+
